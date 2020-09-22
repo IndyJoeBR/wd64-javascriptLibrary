9254ab3cfd60f1ae467342364b2f339f9c37497e
IndyJoe <IndyJoeBRJS@gmail.com>
2020-09-22 00:39:42 -0400

    21 Sept 2020 last evening push

---
+++ 03-API-Fundamentals/3-Fetch/01-Star-Wars/star-wars-scripts.js
@@ -0,0 +1,10 @@
+let starWarsPeopleList = document.querySelector("ul");     // reference to the 'unordered list'
+
+// add the fetch() to the star wars api
+fetch("https://swapi.dev/api/people/")     // 
+  .then(function(response) {
+
+
+  })
+
+
